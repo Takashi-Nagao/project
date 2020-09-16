5635b3003e1ef3ab92093aaef9f1077be5ebf14e
Takashi-Nagao <66946727+Takashi-Nagao@users.noreply.github.com>
2020-09-16 23:58:33 +0900

    add str_name

--- workn/models.py
+++ workn/models.py
@@ -4,3 +4,6 @@ from django.utils import timezone
 class Category(models.Model):
   name = models.CharField('カテゴリー名', max_length=255)
   created_at = models.DateTimeField('作成日', default=timezone.now)
+
+    def __str__(self):
+    return self.name
