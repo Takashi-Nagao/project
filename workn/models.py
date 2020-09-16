ff3d701833265e60680a5e734b05142d028fe3e8
Takashi-Nagao <66946727+Takashi-Nagao@users.noreply.github.com>
2020-09-16 23:59:21 +0900

    add Post model

--- workn/models.py
+++ workn/models.py
@@ -7,3 +7,10 @@ class Category(models.Model):
 
     def __str__(self):
     return self.name
+
+class Post(models.Model):
+
+  title = models.CharField('タイトル', max_length=255)
+  text  = models.TextField('本文')
+  created_at = models.DateTimeField('作成日', default=timezone.now)
+  category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
