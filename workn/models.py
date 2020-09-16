4c0519cf9bc9a07661d043d50dcc4331f33d5880
Takashi-Nagao <66946727+Takashi-Nagao@users.noreply.github.com>
2020-09-16 23:56:45 +0900

    add model Category

--- workn/models.py
+++ workn/models.py
@@ -1,2 +1,6 @@
 from django.db import models
 from django.utils import timezone
+
+class Category(models.Model):
+  name = models.CharField('カテゴリー名', max_length=255)
+  created_at = models.DateTimeField('作成日', default=timezone.now)
