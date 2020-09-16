78ad6c6a6d77c9b2f9ab9678af53bf5e62bd021f
Takashi-Nagao <66946727+Takashi-Nagao@users.noreply.github.com>
2020-09-16 23:59:50 +0900

    add str

--- workn/models.py
+++ workn/models.py
@@ -14,3 +14,6 @@ class Post(models.Model):
   text  = models.TextField('本文')
   created_at = models.DateTimeField('作成日', default=timezone.now)
   category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
+
+  def __str__(self):
+    return self.title
