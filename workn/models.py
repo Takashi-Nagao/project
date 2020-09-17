eaa5903f2c557624b1a5b45302df97103ed0caf8
Takashi-Nagao <66946727+Takashi-Nagao@users.noreply.github.com>
2020-09-17 21:25:07 +0900

    delete model

--- workn/models.py
+++ workn/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 from django.utils import timezone
 
-class Category(models.Model):
-  name = models.CharField('カテゴリー名', max_length=255)
-  created_at = models.DateTimeField('作成日', default=timezone.now)
-
-    def __str__(self):
-    return self.name
-
-class Post(models.Model):
-
-  title = models.CharField('タイトル', max_length=255)
-  text  = models.TextField('本文')
-  created_at = models.DateTimeField('作成日', default=timezone.now)
-  category = models.ForeignKey(Category, verbose_name='カテゴリ', on_delete=models.PROTECT)
-
-  def __str__(self):
-    return self.title
