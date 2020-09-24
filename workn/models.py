2ac857ca43b9512aa87d67627dc4801ff335ab6d
Takashi-Nagao <66946727+Takashi-Nagao@users.noreply.github.com>
2020-09-24 23:44:27 +0900

    Update models.py

--- workn/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Category(models.Model):
-  name = models.CharField('カテゴリー名', max_length=255)
-  created_at = models.DateTimeField('作成日', default=timezone.now)
-
